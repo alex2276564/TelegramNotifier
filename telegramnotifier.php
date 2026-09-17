2112b25ca3440af32ba3576752208de22ad83e6b
alex2276564 <you@example.com>
2026-09-17 23:58:16 +0500

    1.0.10

--- telegramnotifier.php
+++ telegramnotifier.php
@@ -101,7 +101,7 @@ class TelegramNotifier extends Module
     {
         $this->name = 'telegramnotifier';
         $this->tab = 'administration';
-        $this->version = '1.0.9';
+        $this->version = '1.0.10';
         $this->author = 'alex2276564';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => _PS_VERSION_);
